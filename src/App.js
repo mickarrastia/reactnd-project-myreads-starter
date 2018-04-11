d1e5ea5e77e7840da78bf00dcc6a0eb973da1000
mickarrastia <michael.arrastia@gmail.com>
2018-04-11 21:31:20 +0100

    Get all books and display them
    - Add state to BooksApp
    - Set state with BooksAPI.getAll
    - Pass books to ListBooks to render all books retrieved

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Route } from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(
+      (books) => {
+        this.setState(() => ({
+          books: books
+        }))
+      }
+    )
+  }
 
   render() {
     return (
       <div>
-        <Route exact path='/' component={ ListBooks }/>
-        <Route path='/search' component={ SearchBooks }/>
+        <Route exact path='/' render={() => (
+          <ListBooks
+            books={this.state.books}
+          />
+        )}
+        />
+        <Route path='/search' component={SearchBooks}/>
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
 
   render() {
+    const { books } = this.props
     return (
       <div className='list-books'>
         <div className='list-books-title'>
           <h1>My Reads</h1>
         </div>
         <div className='list-books-content'>
-          Books go here
+          <ol className='books-grid'>
+            {books.map((book) => (
+              <li key={book.id}>
+                <div className='book'>
+                  <div className='book-top'>
+                    <div className='book-cover'
+                         style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ol>
         </div>
         <div className='open-search'>
           <Link
